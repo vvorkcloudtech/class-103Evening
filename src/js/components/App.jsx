5d18a038e0a852ea62c337bd9f7e464484b04fd5
talha aslam <muhammadtalha860@gmail.com>
2019-01-28 20:01:17 +0500

    Updated the header

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./header";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div>World</div>
+        <Header/>
       </div>
     );
   }

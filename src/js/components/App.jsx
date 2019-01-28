3d7661b76aa242d3468134d43ddf9f315029e756
talha aslam <muhammadtalha860@gmail.com>
2019-01-28 20:25:32 +0500

    pull

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import Profile from "./Profile.jsx"
-=======
 import Header from "./header";
->>>>>>> 65c1c6d1a9397073aadd8f169be4f9a63c1f4363
 import Header1 from "./Header1"
 
 class App extends React.Component {

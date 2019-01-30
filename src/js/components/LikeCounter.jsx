0492f01ee602181859e18420e9fc4086545de7aa
Sheikh Hammad Ahmed <sheikh.hammd@gmail.com>
2019-01-30 19:28:35 +0500

    test

---
+++ src/js/components/LikeCounter.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+
+class LikeCounter extends Component{
+    render(){
+        return(
+            <div className="LikeCounter"></div>
+        )
+    }
+}
---
+++ src/js/components/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React, {Component } from 'react';
+import $ from 'jquery';
+class LoginForm extends Component{
+    render(){
+        return(
+            <div className="container-fluid MainForm">
+
+            </div>
+        )
+    }
+}
+export default LoginForm;

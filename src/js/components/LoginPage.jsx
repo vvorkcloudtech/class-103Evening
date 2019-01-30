5c7de673fc07fe48099d4703235938a09a0dfda0
JafeelKhanZada <jafeelkhanzada@gmail.com>
2019-01-30 22:56:09 +0500

    Changes

--- src/js/components/LoginPage.jsx
+++ src/js/components/LoginPage.jsx
@@ -3,9 +3,13 @@ import $ from 'jquery';
 class LoginForm extends Component{
     render(){
         return(
-            <div className="container-fluid MainForm">
-
-            
+            <div className="container-fluid MainForm row">
+                <div className="col-2 Left-Content">
+                
+                </div>
+                <div className="col-5 Right-Content">
+                
+                </div>
             </div>
         )
     }

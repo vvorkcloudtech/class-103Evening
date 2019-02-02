1b3755e238c1d40e260031db94b5be30a5b819c0
Sheikh Hammad Ahmed <sheikh.hammd@gmail.com>
2019-02-02 10:33:39 +0500

    DummyData.jsx updated

--- src/js/components/DummyPost.jsx
+++ src/js/components/DummyPost.jsx
@@ -15,7 +15,96 @@ const Post = [
         aurthorDetail:{
             'postAurthorId': 'C001',
             'postAurthorName':'Al Pachino',
-            'postAurthorProfileUrl':'https://www.someDummyPostAurthorProfileUrl.com'
+            'postAurthorProfileUrl':'https://www.someDummyPostAurthorProfileUrl.com',
+            'aurthorProfile':{
+                'aurthorFirstName': 'Al',
+                'aurthorLastName': 'Pachino',
+                'aurthorImageUrl': 'http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-picture-dummy-3.jpg',
+                'aurhorCoverImageUrl': 'https://www.researchgate.net/profile/Robert_Bredereck/publication/260147388/figure/fig1/AS:339839997235203@1458035577476/Reduction-from-Exact-Cover-by-s-Sets-to-Star-Partition-Dummy-vertices-are-in-the-light.png',
+                'noOfLikesOnProfile': '27',
+                'NoOfPosts': '2'
+            }
+        }
+    },
+    {
+        postDetails:{
+            'postId' : 'P001C001',
+            'postUrl': 'https://www.someDummyPostUrl.com',
+            'noOfLikesOnPost': '600',
+            'postTitle' : 'Some Dummy Tittle',
+            'postDescription': 'Some Dummy Description. Some Dummy Description.',
+
+            'postContent':  ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam eligendi placeat  impedit, ex, expedita, porro quae ipsam deleniti nisi fuga voluptas sit magnam rem? Vero nulla natus a ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste.`,
+
+            'postPreviewContent':`Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam eligendi placeat impedit, ex, expedita, porro quae ipsam deleniti nisi fuga voluptas sit magnam rem? Vero nulla natus a ullam.`,
+        },
+        
+        aurthorDetail:{
+            'postAurthorId': 'C001',
+            'postAurthorName':'Al Pachino',
+            'postAurthorProfileUrl':'https://www.someDummyPostAurthorProfileUrl.com',
+            'aurthorProfile':{
+                'aurthorFirstName': 'Al',
+                'aurthorLastName': 'Pachino',
+                'aurthorImageUrl': 'http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-picture-dummy-3.jpg',
+                'aurhorCoverImageUrl': 'https://www.researchgate.net/profile/Robert_Bredereck/publication/260147388/figure/fig1/AS:339839997235203@1458035577476/Reduction-from-Exact-Cover-by-s-Sets-to-Star-Partition-Dummy-vertices-are-in-the-light.png',
+                'noOfLikesOnProfile': '27',
+                'NoOfPosts': '2'
+            }
+        }
+    },
+    {
+        postDetails:{
+            'postId' : 'P001C001',
+            'postUrl': 'https://www.someDummyPostUrl.com',
+            'noOfLikesOnPost': '600',
+            'postTitle' : 'Some Dummy Tittle',
+            'postDescription': 'Some Dummy Description. Some Dummy Description.',
+
+            'postContent':  ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam eligendi placeat  impedit, ex, expedita, porro quae ipsam deleniti nisi fuga voluptas sit magnam rem? Vero nulla natus a ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste.`,
+
+            'postPreviewContent':`Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam eligendi placeat impedit, ex, expedita, porro quae ipsam deleniti nisi fuga voluptas sit magnam rem? Vero nulla natus a ullam.`,
+        },
+        
+        aurthorDetail:{
+            'postAurthorId': 'C001',
+            'postAurthorName':'Al Pachino',
+            'postAurthorProfileUrl':'https://www.someDummyPostAurthorProfileUrl.com',
+            'aurthorProfile':{
+                'aurthorFirstName': 'Al',
+                'aurthorLastName': 'Pachino',
+                'aurthorImageUrl': 'http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-picture-dummy-3.jpg',
+                'aurhorCoverImageUrl': 'https://www.researchgate.net/profile/Robert_Bredereck/publication/260147388/figure/fig1/AS:339839997235203@1458035577476/Reduction-from-Exact-Cover-by-s-Sets-to-Star-Partition-Dummy-vertices-are-in-the-light.png',
+                'noOfLikesOnProfile': '27',
+                'NoOfPosts': '2'
+            }
+        }
+    },
+    {
+        postDetails:{
+            'postId' : 'P001C001',
+            'postUrl': 'https://www.someDummyPostUrl.com',
+            'noOfLikesOnPost': '600',
+            'postTitle' : 'Some Dummy Tittle',
+            'postDescription': 'Some Dummy Description. Some Dummy Description.',
+
+            'postContent':  ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam eligendi placeat  impedit, ex, expedita, porro quae ipsam deleniti nisi fuga voluptas sit magnam rem? Vero nulla natus a ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum modi cum architecto. Quidem, debitis eos quod ea modi numquam. Facere, consequuntur. Quo voluptatem, eaque dolore recusandae quia dolor iste.`,
+
+            'postPreviewContent':`Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam eligendi placeat impedit, ex, expedita, porro quae ipsam deleniti nisi fuga voluptas sit magnam rem? Vero nulla natus a ullam.`,
+        },
+        
+        aurthorDetail:{
+            'postAurthorId': 'C001',
+            'postAurthorName':'Al Pachino',
+            'postAurthorProfileUrl':'https://www.someDummyPostAurthorProfileUrl.com',
+            'aurthorProfile':{
+                'aurthorFirstName': 'Al',
+                'aurthorLastName': 'Pachino',
+                'aurthorImageUrl': 'http://profilepicturesdp.com/wp-content/uploads/2018/07/profile-picture-dummy-3.jpg',
+                'aurhorCoverImageUrl': 'https://www.researchgate.net/profile/Robert_Bredereck/publication/260147388/figure/fig1/AS:339839997235203@1458035577476/Reduction-from-Exact-Cover-by-s-Sets-to-Star-Partition-Dummy-vertices-are-in-the-light.png',
+                'noOfLikesOnProfile': '27',
+                'NoOfPosts': '2'
+            }
         }
     }
 ]
